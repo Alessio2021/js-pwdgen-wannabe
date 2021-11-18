fcce9bb605c8918db46991ae318a5c1ca40bd528
Alessio2021 <alessiobut1911@gmail.com>
2021-11-18 15:29:02 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -8,15 +8,15 @@ const name = prompt("Inserisci il tuo Nome");
 const surname = prompt("Inserisci il tuo Cognome");
 const favouriteColor = prompt("Inserisci il tuo colore preferito");
 
-const fixNumber = (7 * 3)
+const fixNumber = (7 * 3);
 
 console.log(name + " " + surname + " " + favouriteColor + " " + fixNumber);
 
-let password = name + surname + favouriteColor + fixNumber
+let password = name + surname + favouriteColor + fixNumber;
 
 let text = `
 <h1>Benvenuto su Boolword Generator,</h1> 
 ${name} ${surname}, grazie ai suoi dati e al suo colore preferito, il ${favouriteColor}, la password suggerita e' ${password}. Si ricordi di non mostrare o comunicare la tua password a nessuno, per la tua sicurezza.
 `;
 
-document.getElementById("first_java").innerHTML = text
+document.getElementById("first_java").innerHTML = text;
